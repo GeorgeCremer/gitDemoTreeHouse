7b4279c8ec83c5466d234827c32e6340d108c3b5
George.Cremer <george.cremer@gmail.com>
2016-08-09 11:40:15 +0100

    Initial Commit

---
+++ GitDemoTH/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitDemoTH
+//
+//  Created by George Cremer on 09/08/2016.
+//  Copyright © 2016 GeorgeCremer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
